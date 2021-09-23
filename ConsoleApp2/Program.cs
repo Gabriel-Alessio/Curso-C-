478aa9fe3eb941bdc401390f1510a977f71187fe
gabryelalessio <gabryelalessio@gmail.com>
2021-09-23 10:20:53 -0300

    Atv 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp2
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      int n = int.Parse(Console.ReadLine());
+
+      int[] valor;
+      valor = new int[n];
+      string[] v = Console.ReadLine().Split(' ');
+      int posicao = 0;
+
+      for (var i = 0; i < n; i++)
+      {
+        valor[i] = int.Parse(v[i]); // atribui valores no vetor
+
+        if(double.Parse(v[i]) % 2 == 0)
+        {
+          Console.Write(valor[i] + " ");
+          posicao++;
+        }
+      }
+      Console.WriteLine();
+      Console.WriteLine(posicao);
     }
   }
 }
