6b811ebd2f41c15e64e69c14ba8f2d4cf36f04d1
gabryelalessio <gabryelalessio@gmail.com>
2021-09-23 17:57:06 -0300

    Atv 1 e 2 matriz

---
+++ Matriz Atv 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matriz_Atv_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] aux = Console.ReadLine().Split(' ');
+      int m = int.Parse(aux[0]);
+      int n = int.Parse(aux[1]);
+
+      int[,] mat = new int[m, n];
+
+      for (var i = 0; i < m; i++)
+      {
+        aux = Console.ReadLine().Split(' ');
+        for (var j = 0; j < n; j++)
+        {
+          mat[i, j] = int.Parse(aux[j]); //adiciona os valores nas linha i e na linha j
+        }
+      }
+      Console.WriteLine("VALORES NEGATIVOS: ");
+      for (var i = 0; i < m; i++)
+      {
+        for (var j = 0; j < n; j++)
+        {
+          if (mat[i, j] < 0)
+          {
+            Console.WriteLine(mat[i, j]); 
+          }
+        }
+      }
+    }
+  }
+}
---
+++ Matriz Atv 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Matriz_Atv_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int n = int.Parse(Console.ReadLine());
+
+      int[,] mat = new int[n, n];
+
+      for (var i = 0; i < n; i++)
+      {
+        string[] aux = Console.ReadLine().Split(' ');
+        for (var j = 0; j < n; j++)
+        {
+          mat[i, j] = int.Parse(aux[j]); //adiciona os valores nas linha i e na linha j
+        }
+      }
+      for (var i = 0; i < n; i++)
+      {
+        var sum = 0;
+        for (var j = 0; j < n; j++)
+        {
+          sum += mat[i, j]; // percorre todos os valores e vai fazer a soma de todas as linhas 
+        }
+        Console.WriteLine(sum);
+      }
+    }
+  }
+}
